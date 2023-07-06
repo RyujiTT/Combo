af963f5e7d82aabb7b74ba67afafff7af3e6c46e
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-07-06 08:40:56 +0000

    [add]controllerとhomesのtopとヘッダー

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,25 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :users
+  namespace :public do
+    get 'homes/top'
+  end
+  # 顧客用
+  # URL /customers/sign_in ...
+  devise_for :users,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+
+  scope module: :public do
+  end
+
+  root to: 'public/homes#top'
+  get 'about' => 'public/homes#about', as: 'about'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
